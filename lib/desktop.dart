b4f2ca6ccc3f7dab9e8e96201729da84e1657149
hasanMohamed99 <hasanmohamed09@gmail.com>
2023-01-31 20:00:46 +0200

    add desktop code

--- lib/desktop.dart
+++ lib/desktop.dart
@@ -15,17 +15,70 @@ class DesktopScreen extends StatelessWidget {
             ),
           ),
           Expanded(
-            child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'Email Address',
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Login Now',
+                    style: Theme.of(context).textTheme.headlineMedium,
                   ),
-                ),
-              ],
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Email Address',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Password',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                        child: MaterialButton(
+                          height: 45,
+                          onPressed: () {},
+                          color: Colors.teal,
+                          textColor: Colors.white,
+                          child: const Text(
+                            'LOGIN',
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 20.0,
+                      ),
+                      Expanded(
+                        child: MaterialButton(
+                          height: 45,
+                          onPressed: () {},
+                          color: Colors.teal[700],
+                          textColor: Colors.white,
+                          child: const Text(
+                            'REGISTER',
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          )
+          ),
         ],
       ),
     );
